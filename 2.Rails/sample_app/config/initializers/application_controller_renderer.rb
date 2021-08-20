a6e72c2c50f3bf0b6fe80964acb5cba8d24e5527
MasatoArima <afoc.0707@gmail.com>
2021-08-20 10:15:54 +0000

    [finish]2-7

--- 2.Rails/sample_app/config/initializers/application_controller_renderer.rb
+++ 2.Rails/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '58600f6345ef48ed7b78c03e724e4fb8aa983843be377fa0dc1b2219463283403e076d8e0e0f490a78dfeb8c6fb2163424571490eaad9154a9c2f5fc1c535f50'

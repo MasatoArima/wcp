db176520f4a452167974db9fdfd64222f9d87693
MasatoArima <afoc.0707@gmail.com>
2021-08-29 02:51:16 +0000

    [finish]4-3

--- 4.application_2/meshiterro/config/initializers/application_controller_renderer.rb
+++ 4.application_2/meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a9b1d2c559037a747555d759006f10d469cfe8a9ecae795e7e30109b8a0e3b2ff76b0242c53de9bde2613ade98dcedf8f9b9f9de6be286c4460b17085bc2a906'
